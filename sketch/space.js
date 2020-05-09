92d8d78f769eba2c22e9a049d70c0debfbaf1bd8
mwchoo <cmw1408@u.sogang.ac.kr>
2020-05-10 03:01:25 +0900

    add 3d exhibition space

--- sketch/space.js
+++ sketch/space.js
@@ -1,30 +1,34 @@
 function drawSpace() {
   push();
-  translate(0, 1430, 50);
-  rotateX(planet_rot);
-  rotateY(planet_self_rot);
-  noStroke();
+  translate(0, -500, 800);
   fill(150, 200, 100);
-  texture(earth_texture);
-  sphere(1250);
 
-  // draw stars
-  for (let i = 0; i < 80; i++) {
-    v = random3D();
-    // PVector v = PVector.random3D(this);
-    v.mult(2500);
-    stroke('rgb(195, 195, 195)');
-    strokeWeight(round(random(1, 10)));
-    point(v.x, v.y, v.z);
-  }
+  // bottom
+  //texture(textureMat.concrete);
+  noStroke();
+
+  // front-back
+  push();
+  translate(0, 0, -1500);
+  plane(4000, 2000);
   pop();
-}
+  rotateX(HALF_PI);
+  translate(0, 1000, -1000);
+  plane(4000, 5000);
 
-function random3D() {
-  const angle = random(0, TWO_PI);
-  const vz = random(-1, 1);
-  const vx = sqrt(1-vz*vz)*cos(angle);
-  const vy = sqrt(1-vz*vz)*sin(angle);
-  v = new p5.Vector(vx, vy, vz);
-  return v;
+  // left
+  rotateY(HALF_PI);
+  translate(-1000, 0, -2000);
+  plane(2000, 5000);
+
+  // top
+  rotateY(HALF_PI);
+  translate(-2000, 0, -1000);
+  plane(4000, 5000);
+
+  // right
+  rotateY(HALF_PI);
+  translate(-1000, 0, -2000);
+  plane(2000, 5000);
+  pop();
 }

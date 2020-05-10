31417d2de2c2ada4081dae8272fca3456920b596
mwchoo <cmw1408@u.sogang.ac.kr>
2020-05-11 03:26:40 +0900

    complete car mode

--- sketch/car.js
+++ sketch/car.js
@@ -1,4 +1,4 @@
-class Car {
+class Car {  // Implemented by Minwoo Choo
   constructor(car_size, car_color) {
     this.car_size = car_size;
     this.car_color = car_color;
@@ -17,10 +17,45 @@ class Car {
       'right_white': true
     }
     this.door_wing = {
-      'mode': 0, // [0]: close, [1]: open, [2] dance
-      'rot_angle': 0,
-      'rot_speed': 0.5,
-      'dancing_factor': 1
+      'mode': 0, // [0]: close, [1]: open, [2] dance, [3] stop
+      'rot_angle': 6.28,  // open(6.28~4.71), close(4.71~6.28), dancing(4.66~5.33)
+      'rot_speed': 0.01,
+      'dancing_factor': 1,
+      'direction': true
+    }
+  }
+
+  turnOffAllLight() {
+    this.head_light = {
+      'left1': false,
+      'left2': false,
+      'left3': false,
+      'right1': false,
+      'right2': false,
+      'right3': false
+    }
+    this.fog_light = {
+      'left_yellow': false,
+      'left_white': false,
+      'right_yellow': false,
+      'right_white': false
+    }
+  }
+
+  turnOnAllLight() {
+    this.head_light = {
+      'left1': true, // default head light
+      'left2': false,
+      'left3': false,
+      'right1': false,
+      'right2': true, // default head light
+      'right3': false
+    }
+    this.fog_light = {
+      'left_yellow': true,
+      'left_white': true,
+      'right_yellow': true,
+      'right_white': true
     }
   }
 
@@ -61,22 +96,45 @@ class Car {
   }
 
   handleDoorWing() {
-    const {mode, rot_angle} = this.door_wing;
+    const {mode, rot_angle, rot_speed, dancing_factor, direction} = this.door_wing;
     if (mode === 0) {  // door close
-      if (rot_angle > 0) {
-        this.door_wing.rot_angle -= 0.2;
+      if (rot_angle < 6.28) {
+        this.door_wing.rot_angle += 0.01;
       }
     } else if (mode === 1) {  // door open
-      if (rot_angle < 90) {
-        this.door_wing.rot_angle += 0.2;
+      if (rot_angle > 4.71) {
+        this.door_wing.rot_angle -= 0.001;
       }
     } else if (mode === 2) {  // dancing
       // add dancing mode
+      if (direction) {
+        if (rot_angle > 4.66 * dancing_factor) {
+          this.door_wing.rot_angle -= rot_speed;
+        } else {
+          this.door_wing.direction = !direction;
+        }
+      } else {
+        if (rot_angle < 5.33 * dancing_factor) {
+          this.door_wing.rot_angle += rot_speed;
+        } else {
+          this.door_wing.direction = !direction;
+        }
+      }
     }
 
+    push();
+    translate(-10, 70, 0);
+    rotateZ(this.door_wing.rot_angle);
+    translate(10, -70, 0);
     model(models.car_door_l);
+    pop();
 
+    push();
+    translate(10, 70, 0);
+    rotateZ(-this.door_wing.rot_angle);
+    translate(-10, -70, 0);
     model(models.car_door_r);
+    pop();
   }
 
   display() {
